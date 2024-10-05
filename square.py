e4a8e449590a2934f66c1ee05ff91e12a75f996c
amithsoddy <anithasoddy@gmail.com>
2024-10-05 18:37:54 +0530

    turtle

---
+++ square.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+s=turtle.Screen()
+s.setup(400,300)
+turtle.title("let begin")
+pen=turtle.Turtle()
+for i in range(4):
+    pen.forward(100)
+    pen.left(90)
+    i=i+1
+turtle.done()
